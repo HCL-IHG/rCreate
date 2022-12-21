e50e930f751d58d39dc57be15401d44c38dfbaa3
Parmy <hcl.ihg@gmail.com>
2022-12-21 20:56:56 +0000

    Remote file demonstration

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,29 @@
 # Recipe:: default
 #
 # Copyright:: 2022, The Authors, All Rights Reserved.
+#
+# Cookbook:: cCreate
+# Recipe:: default
+#
+# Copyright:: 2022, The Authors, All Rights Reserved.
+#
+# Cookbook:: tCreate
+# Recipe:: default
+#
+# Copyright:: 2022, The Authors, All Rights Reserved.
+#
+# Cookbook:: fCreate
+# Recipe:: default
+#
+# Copyright:: 2022, The Authors, All Rights Reserved.
+#node['ipaddress']
+#node['memory']['total']
+
+#print statement I have 4 apples
+apple_count = 4
+puts "I have #{apple_count} apples"
+
+remote_file '/root/fCreate.txt' do
+  source 'http://www.sbeams.org/sample_data/Microarray/External_test_data.tar.gz'
+end
+#include_recipe 'fUpdate::uRecipe'
